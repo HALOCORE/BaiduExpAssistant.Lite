ca1373af28af49b159a7461acdde23db381e8808
Bo Wang <wbprosci@outlook.com>
2020-05-01 21:35:08 +0800

    pic unloader hacked.

--- ExpA.Lite/tools/AllPoly.js
+++ ExpA.Lite/tools/AllPoly.js
@@ -32,12 +32,59 @@ try {
             window.external._getImageDict[url]['onFailed'](message);
         } catch(e) { console.error(e); }
     }
+    
+    //hacking pic uploader.
+    console.log("# hacking pic uploader...");
+    var criticalElem = document.getElementsByClassName("webuploader-element-invisible")[0];
+    function cloneNodeGen(clone) {
+        return function() {
+            var ret = clone.apply(this, arguments);
+            console.log("# clone: " + ret.tagName.toLowerCase());
+            ret.cloneNode = cloneNodeGen(ret.cloneNode);
+            ret.addEventListener = function(adder) {
+                return function() {
+                    var ret = adder.apply(this, arguments);
+                    console.log("# addEventListener.", arguments);
+                    if(arguments[0] === "change") {
+                        console.log("# change EventListener found.", arguments[1]);
+                        window._onUploadFileChange = arguments[1];
+                    }
+                    return ret;
+                }
+            }(ret.addEventListener);
+            return ret;
+        };
+    }
+    criticalElem.cloneNode = cloneNodeGen(criticalElem.cloneNode);
+
+    function dataURLtoFakeFile(dataurl, filename) {
+        var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+            bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+        while(n--){
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+        let file = new Blob([u8arr], { type: mime });
+        file.lastModifiedDate = new Date();
+        file.name = filename;
+        return file;
+    }
+    //window._onUploadFileChange({type:"change", target:{"files": [fakeFile]}})
 } catch (e) {
     console.log("浏览器环境");
     window._EXTENSION_IS_CHROME_ = true;
     console.log(">>> Pollyfill window.external.notify");
     window.external.notify = function (msg) {
         console.log("[window.external.notify]", msg);
+    }
+
+    function dataURLtoFile(dataurl, filename) {
+        var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+            bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+        while(n--){
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+        let file =new File([u8arr], filename, {type:mime});
+        return file;
     }
 }
 
